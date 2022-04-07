2dd3048d3370fb65f4683fd5236a618176fc00b2
Denzel <denzelchung@hotmail.com>
2022-04-07 19:56:38 +0800

    Add calculation logic

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders app", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Calculation App/i)).toBeInTheDocument();
+
+  const input1: HTMLInputElement = screen.getByLabelText(/Input 1/i);
+  const input2: HTMLInputElement = screen.getByLabelText(/Input 2/i);
+  expect(input1.value).toBe("0");
+  expect(input2.value).toBe("0");
+  expect(screen.queryByText(/Results/i)).not.toBeInTheDocument();
+});
+
+test("renders results on add", () => {
+  render(<App />);
+  const input1: HTMLInputElement = screen.getByLabelText(/Input 1/i);
+  const input2: HTMLInputElement = screen.getByLabelText(/Input 2/i);
+
+  fireEvent.change(input1, {
+    target: { value: "100" },
+  });
+  expect(input1.value).toBe("100");
+
+  fireEvent.change(input2, {
+    target: { value: "-1" },
+  });
+  expect(input2.value).toBe("-1");
+
+  fireEvent(
+    screen.getByText("Add"),
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+  expect(screen.getByText(/Results/i)).toBeInTheDocument();
+  expect(screen.getByTestId("result").textContent).toBe("99");
+});
+
+test("renders results on subtract", () => {
+  render(<App />);
+  const input1: HTMLInputElement = screen.getByLabelText(/Input 1/i);
+  const input2: HTMLInputElement = screen.getByLabelText(/Input 2/i);
+
+  fireEvent.change(input1, {
+    target: { value: "8" },
+  });
+  expect(input1.value).toBe("8");
+
+  fireEvent.change(input2, {
+    target: { value: "100" },
+  });
+  expect(input2.value).toBe("100");
+
+  fireEvent(
+    screen.getByText("Subtract"),
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+  expect(screen.getByText(/Results/i)).toBeInTheDocument();
+  expect(screen.getByTestId("result").textContent).toBe("-92");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [inputs, setInputs] = useState({ number1: "0", number2: "0" });
+  const [result, setResult] = useState<number | null>(null);
+
+  function onInputChange(event: { target: HTMLInputElement }) {
+    setInputs((state) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  function onAdd() {
+    setResult(parseInt(inputs.number1) + parseInt(inputs.number2));
+  }
+
+  function onSubtract() {
+    setResult(parseInt(inputs.number1) - parseInt(inputs.number2));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        <h1>Calculation App</h1>
+        <label>
+          Input 1:
+          <input
+            type="number"
+            name="number1"
+            value={inputs.number1}
+            onChange={onInputChange}
+          />
+        </label>
+        <label>
+          Input 2:
+          <input
+            type="number"
+            name="number2"
+            value={inputs.number2}
+            onChange={onInputChange}
+          />
+        </label>
+
+        <button onClick={onAdd}>Add</button>
+        <button onClick={onSubtract}>Subtract</button>
+
+        {result != null && (
+          <div>
+            Results: <span data-testid="result">{result}</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
